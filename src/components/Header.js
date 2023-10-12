5dc489951988ec5dcfdc85dce82e116e88b54186
John Doe <gnoudeath@gmail.com>
2023-10-12 14:35:49 +0700

    submit

--- src/components/Header.js
+++ src/components/Header.js
@@ -132,7 +132,7 @@ const Header = () => {
                 <div className="menu-links">
                   <div className="d-flex align-items-center gap-15">
                     <NavLink to="/">Home</NavLink>
-                    <NavLink to="/">Our Store</NavLink>
+                    <NavLink to="/ourStone">Our Store</NavLink>
                     <NavLink to="/">Blogs</NavLink>
                     <NavLink to="/contact">Contact</NavLink>
                   </div>
---
+++ src/pages/OurStone.js
@@ -0,0 +1,130 @@
+import React from "react";
+import BreadCrumb from "../components/BreadCrumb";
+import Meta from "../components/Meta";
+
+const OurStone = () => {
+  return (
+    <>
+      <Meta title={"Our Stone"} />
+      <BreadCrumb title="Our Stone" />
+      <div className="store-wrapper home-wrapper-2 py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-3">
+              <div className="filter-card mb-3">
+                <h3 className="filter-title">Shop by Categories</h3>
+                <div>
+                  <ul className="ps-0">
+                    <li>Watch</li>
+                    <li>TV</li>
+                    <li>Camera</li>
+                    <li>Laptop</li>
+                  </ul>
+                </div>
+              </div>
+              <div className="filter-card mb-3">
+                <h3 className="filter-title">Filter by</h3>
+                <div>
+                  <h5 className="sub-title">Availablity</h5>
+                  <div>
+                    <div className="form-check">
+                      <input
+                        className="form-check-input"
+                        type="checkbox"
+                        value=""
+                      ></input>
+                      <label className="form-check-lable" htmlFor="">
+                        In Stock (1)
+                      </label>
+                    </div>
+                    <div className="form-check">
+                      <input
+                        className="form-check-input"
+                        type="checkbox"
+                        value=""
+                        id=""
+                      ></input>
+                      <label className="form-check-lable" htmlFor="">
+                        Out of Stock (0)
+                      </label>
+                    </div>
+                  </div>
+                  <h5 className="sub-title">Price</h5>
+                  <div className="d-flex align-items-center gap-10">
+                    <div className="form-floating">
+                      <input
+                        type="email"
+                        className="form-control py-1"
+                        id="floatingInput"
+                        placeholder="From"
+                      />
+                      <label htmlFor="floatingInput">$ From:</label>
+                    </div>
+                    <div className="form-floating">
+                      <input
+                        type="email"
+                        className="form-control py-1"
+                        id="floatingInput1"
+                        placeholder="To"
+                      />
+                      <label htmlFor="floatingInput1">$ To:</label>
+                    </div>
+                  </div>
+                  <h5 className="sub-title">Colors</h5>
+                  <div>
+                    <ul className="colors ps-0">
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                    </ul>
+                  </div>
+                  <h5 className="sub-title">Size</h5>
+                  <div>
+                  <div className="form-check">
+                      <input
+                        className="form-check-input"
+                        type="checkbox"
+                        value="color-1"
+                      ></input>
+                      <label className="form-check-lable" htmlFor="color-1">
+                        S (2)
+                      </label>
+                    </div>
+                    <div className="form-check">
+                      <input
+                        className="form-check-input"
+                        type="checkbox"
+                        value="color-2"
+                      ></input>
+                      <label className="form-check-lable" htmlFor="color-2">
+                        M (2)
+                      </label>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="filter-card mb-3">
+                <h3 className="filter-title">Product Tags</h3>
+              </div>
+              <div className="filter-card mb-3">
+                <h3 className="filter-title">Random Product</h3>
+              </div>
+            </div>
+            <div className="col-9"></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default OurStone;
