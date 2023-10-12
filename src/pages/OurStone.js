adc73c1abbc025e4179064a020aee65488a5fd29
John Doe <gnoudeath@gmail.com>
2023-10-12 15:29:51 +0700

    submit

--- src/pages/OurStone.js
+++ src/pages/OurStone.js
@@ -1,6 +1,7 @@
 import React from "react";
 import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
+import ReactStars from "react-rating-stars-component";
 
 const OurStone = () => {
   return (
@@ -85,11 +86,44 @@ const OurStone = () => {
                       <li></li>
                       <li></li>
                       <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
                     </ul>
                   </div>
                   <h5 className="sub-title">Size</h5>
                   <div>
-                  <div className="form-check">
+                    <div className="form-check">
                       <input
                         className="form-check-input"
                         type="checkbox"
@@ -109,17 +143,152 @@ const OurStone = () => {
                         M (2)
                       </label>
                     </div>
+                    <div className="form-check">
+                      <input
+                        className="form-check-input"
+                        type="checkbox"
+                        value="color-2"
+                      ></input>
+                      <label className="form-check-lable" htmlFor="color-2">
+                        L (2)
+                      </label>
+                    </div>
+                    <div className="form-check">
+                      <input
+                        className="form-check-input"
+                        type="checkbox"
+                        value="color-2"
+                      ></input>
+                      <label className="form-check-lable" htmlFor="color-2">
+                        XL (1)
+                      </label>
+                    </div>
+                    <div className="form-check">
+                      <input
+                        className="form-check-input"
+                        type="checkbox"
+                        value="color-2"
+                      ></input>
+                      <label className="form-check-lable" htmlFor="color-2">
+                        XXL (2)
+                      </label>
+                    </div>
                   </div>
                 </div>
               </div>
               <div className="filter-card mb-3">
                 <h3 className="filter-title">Product Tags</h3>
+                <div className="product-tags d-flex flex-wrap align-items-center gap-10">
+                  <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                    Headphone
+                  </span>
+
+                  <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                    Laptop
+                  </span>
+
+                  <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                    Moblie
+                  </span>
+
+                  <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                    Oppo
+                  </span>
+
+                  <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                    Speaker
+                  </span>
+                  <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                    Tablet
+                  </span>
+                  <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                    Apple
+                  </span>
+                  <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                    Samsung
+                  </span>
+                </div>
               </div>
               <div className="filter-card mb-3">
                 <h3 className="filter-title">Random Product</h3>
+                <div>
+                  <div className="random-products mb-3 d-flex">
+                    <div className="w-50">
+                      <img
+                        src="images/headphone.jpg"
+                        className="img-fluid"
+                        alt="headphone"
+                      ></img>
+                    </div>
+                    <div className="w-50">
+                      <h5>Kids Headphone bulk 10 pack multi colored for...</h5>
+                      <ReactStars
+                        count={5}
+                        size={24}
+                        value={4}
+                        edit={false}
+                        activeColor="#ffd700"
+                      />
+                      <b>$ 300</b>
+                    </div>
+                  </div>
+                  <div className="random-products d-flex">
+                    <div className="w-50">
+                      <img
+                        src="images/watch.jpg"
+                        className="img-fluid"
+                        alt="watch"
+                      ></img>
+                    </div>
+                    <div className="w-50">
+                      <h5>APPLE Watch Series 2 - 42 Mn Stainless Steel...</h5>
+                      <ReactStars
+                        count={5}
+                        size={24}
+                        value={4}
+                        edit={false}
+                        activeColor="#ffd700"
+                      />
+                      <b>$ 300</b>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
-            <div className="col-9"></div>
+            <div className="col-9">
+              <div className="filter-sort-grid">
+                <div className="d-flex justify-content-between align-items-center">
+                  <div className="d-flex align-items-center gap-10">
+                    <p className="mb-0 d-block">Sort By:</p>
+                    <select name="" className="form-control form-select" id="">
+                      <option value="manual">Featured</option>
+                      <option value="best-selling" selected="selected">
+                        Best selling
+                      </option>
+                      <option value="title-ascending">
+                        Alphabetically, A-Z
+                      </option>
+                      <option value="title-descending">
+                        Alphabetically, Z-A
+                      </option>
+                      <option value="price-ascending">
+                        Price, low to high
+                      </option>
+                      <option value="price-descending">
+                        Price, high to low
+                      </option>
+                      <option value="created-ascending">
+                        Date, old to new
+                      </option>
+                      <option value="created-descending">
+                        Date, new to old
+                      </option>
+                    </select>
+                  </div>
+                  <div></div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
