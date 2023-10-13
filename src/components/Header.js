cabe3b9d560bf1c9d84520f8fd12188d0e116527
John Doe <gnoudeath@gmail.com>
2023-10-13 15:35:14 +0700

    Compare product

--- src/components/Header.js
+++ src/components/Header.js
@@ -49,7 +49,7 @@ const Header = () => {
             <div className="col-5">
               <div className="header-upper-links d-flex align-items-center justify-content-between">
                 <div>
-                  <Link className="d-flex align-items-center gap-10 text-white">
+                  <Link to='/compare-product' className="d-flex align-items-center gap-10 text-white">
                     <img src="images/compare.svg" alt="compare"></img>
                     <p className="mb-0">
                       Compare <br /> Product
@@ -57,7 +57,7 @@ const Header = () => {
                   </Link>
                 </div>
                 <div>
-                  <Link className="d-flex align-items-center gap-10 text-white">
+                  <Link to='/wishlist' className="d-flex align-items-center gap-10 text-white">
                     <img src="images/wishlist.svg" alt="wishlist"></img>
                     <p className="mb-0">
                       Favourite <br /> Wishlist
@@ -65,7 +65,7 @@ const Header = () => {
                   </Link>
                 </div>
                 <div>
-                  <Link className="d-flex align-items-center gap-10 text-white">
+                  <Link to='/login' className="d-flex align-items-center gap-10 text-white">
                     <img src="images/user.svg" alt="user"></img>
                     <p className="mb-0">
                       Login <br /> My Account
@@ -73,7 +73,7 @@ const Header = () => {
                   </Link>
                 </div>
                 <div>
-                  <Link className="d-flex align-items-center gap-10 text-white">
+                  <Link to='/cart' className="d-flex align-items-center gap-10 text-white">
                     <img src="images/cart.svg" alt="cart"></img>
                     <p>
                       <div className="d-flex flex-column gap-10">
--- src/pages/OurStone.js
+++ src/pages/OurStone.js
@@ -3,6 +3,7 @@ import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
 import ReactStars from "react-rating-stars-component";
 import ProductCard from "../components/ProductCard"
+import Color from "../components/Color";
 const OurStone = () => {
     const [grid,setGrid] = useState(4);
     // alert(grid);
@@ -75,53 +76,7 @@ const OurStone = () => {
                   </div>
                   <h5 className="sub-title">Colors</h5>
                   <div>
-                    <ul className="colors ps-0">
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                    </ul>
+                    <Color />
                   </div>
                   <h5 className="sub-title">Size</h5>
                   <div>
